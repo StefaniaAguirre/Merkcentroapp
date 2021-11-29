7f2ad0fe62cd346ba314035d41dd807005f6b82e
Nicolox <nmurilloc@uqvirtual.edu.co>
2021-11-28 21:41:21 -0500

    Se comienzan los servicios

--- src/app/horarios/horarios.service.ts
+++ src/app/horarios/horarios.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { horario } from './horarios.type';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { BehaviorSubject, Observable, of,throwError } from 'rxjs';
+import { map, switchMap, tap, take } from 'rxjs/operators';
+import { __values } from 'tslib';
+import { cloneDeep, result } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HorariosService {
 
-  constructor() { }
+  private _horarios:BehaviorSubject<horario[]> = new BehaviorSubject<horario[]>([]);
+  private _horario: BehaviorSubject<horario | null> = new BehaviorSubject<horario | null>(null);
+  private _horarioById:BehaviorSubject<horario[] | null> = new BehaviorSubject<horario[] | null>(null);
+
+  constructor(private db: AngularFirestore) { }
+
+
+
+ /**
+   * 
+   * @param horariosForm 
+   * @param addEvento 
+   * @returns 
+   */
+  crearEvento(horariosForm: any): Promise<any> {
+    
+    var horaEntrada: string = horariosForm.horaEntrada;
+    var horaSalida: string = horariosForm.horaSalida;
+    var empleado: string = horariosForm.empleado;
+    var mensaje: string = horariosForm.mensaje;
+  
+    
+
+
+
+
+    console.log(horaEntrada, horaSalida, empleado, mensaje)
+    return this.db.collection('horarios').add({ horaEntrada, horaSalida, empleado, mensaje}).then(results => {
+
+      console.log(results);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
 }
--- src/app/horarios/horarios.type.ts
+++ src/app/horarios/horarios.type.ts
@@ -1,9 +1,14 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
 import { FormControl } from "@angular/forms";
 
 
 export interface horario
 {
 id: string;
+horaEntrada: string;
+horaSalida: string;
+empleado: string;
+mensaje: string;
 
 
 }
